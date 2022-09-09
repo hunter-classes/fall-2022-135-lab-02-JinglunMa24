69d6e6a90592b8aead2278075f473c91fb3633cf
Jinglun Ma <2461447099ma@gmail.com>
2022-09-08 21:08:06 -0400

    added

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 
 int main () {
 	print_interval(-5, 10);
+	std::cout << std::endl;
 
+	print_interval(0, 100);
+	std::cout << std::endl;
+
+	print_interval(-100, 0);
+	std::cout << std::endl;
 	return 0;
 }
